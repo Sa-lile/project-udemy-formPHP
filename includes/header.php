3217480f0aac1a252915725d9ea4c7b33bd099af
sachiyo <sachiyo.sable@gmail.com>
2023-01-23 13:21:15 +0100

    Crée footer et header

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'includes/database.php';
+?>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css" </head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</li>
+                <li><a href="login.php">Login</li>
+                <li><a href="register.php">Register</li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+<body>
--- index.php
+++ index.php
@@ -1,25 +1,9 @@
 <?php
-require_once 'includes/database.php';
+require_once 'includes/header.php'
 ?>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    $sql = "SELECT * FROM users";
-    $result = mysqli_query($conn, $sql);
-    $rowCount = mysqli_num_rows($result);
 
-    if ($rowCount > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo $row['username'] . "<br>";   // Afficher le BDD de username
-        }
-    } else {
-        echo "No results found";
-    }
-    ?>
-</body>
+HOME
 
-</html>
+<?php
+require_once 'includes/footer.php'
+?>
